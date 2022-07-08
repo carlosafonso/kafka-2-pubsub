cb33d4cb59c18f050e201a654e52447b6e501eae
Carlos Afonso <fonsi@google.com>
2022-07-08 16:39:37 +0000

    Hydrate the Terraform vars file on deploy

--- scripts/10_hydrate_config.sh
+++ scripts/10_hydrate_config.sh
@@ -4,3 +4,9 @@ set -euo pipefail
 
 envsubst < ./infra/k8s/kafka.yaml.dist > ./infra/k8s/kafka.yaml
 envsubst < ./kafka-connect/pubsub-connector.json.dist > ./kafka-connect/pubsub-connector.json
+
+cat > ./infra/terraform.tfvars <<-EOF
+    project = "${GOOGLE_CLOUD_PROJECT}"
+    region = "us-central1"
+    zone = "us-central1-a"
+EOF
