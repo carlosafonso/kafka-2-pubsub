ddc8ac5df926ae10f8221d6bfbc7a36acac459a9
Carlos Afonso <fonsi@google.com>
2022-07-08 12:58:10 +0000

    Add GKE cluster and Kubernetes manifests

--- infra/main.tf
+++ infra/main.tf
@@ -4,10 +4,20 @@ terraform {
       source  = "hashicorp/google"
       version = "4.27.0"
     }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "4.27.0"
+    }
   }
 }
 
 provider "google" {
+  project = var.project
+  region  = var.region
+  zone    = var.zone
+}
+
+provider "google-beta" {
   project = var.project
   region  = var.region
   zone    = var.zone
@@ -22,7 +32,7 @@ resource "google_compute_subnetwork" "subnetwork" {
   name = "kafka-2-pubsub"
   ip_cidr_range = "10.0.0.0/24"
   region = var.region
-  network = google_compute_network.vpc.id   
+  network = google_compute_network.vpc.id
 }
 
 resource "google_compute_router" "router" {
@@ -68,7 +78,7 @@ resource "google_pubsub_subscription" "subscription_b" {
   topic = google_pubsub_topic.topic.name
 }
 
-resource "google_service_account" "kafka" {
+/*resource "google_service_account" "kafka" {
   account_id = "kafka-2-pubsub"
 }
 
@@ -188,4 +198,54 @@ value.converter.schemas.enable=false" > /home/kafka/cps-sink-connector.propertie
 
     echo "Initialization complete: run sudo -u kafka /home/kafka/run-pubsub-kafka-connector.sh when ready"
   EOF
+} */
+
+resource "google_artifact_registry_repository" "my-repo" {
+  provider = google-beta
+  location = var.region
+  repository_id = "kafka-2-pubsub"
+  format = "DOCKER"
+}
+
+resource "google_service_account" "gke" {
+  account_id   = "kafka-2-pubsub-gke"
+  display_name = "kafka2pubsub - Service account for the GKE cluster that allows it to interact with Pub/Sub"
+}
+
+resource "google_project_iam_member" "gke_pubsub_publisher" {
+  project = var.project
+  role = "roles/pubsub.publisher"
+  member = "serviceAccount:${google_service_account.gke.email}"
+}
+
+resource "google_project_iam_member" "gke_artifact_registry_viewer" {
+  project = var.project
+  role = "roles/artifactregistry.reader"
+  member = "serviceAccount:${google_service_account.gke.email}"
+}
+
+resource "google_container_cluster" "cluster" {
+  name = "kafka-2-pubsub"
+  location = var.region
+  network = google_compute_network.vpc.self_link
+  subnetwork = google_compute_subnetwork.subnetwork.self_link
+
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  networking_mode = "VPC_NATIVE"
+  ip_allocation_policy {}
+}
+
+resource "google_container_node_pool" "nodepool" {
+  name = "kafka-2-pubsub"
+  cluster = google_container_cluster.cluster.id
+  node_count = 1
+
+  node_config {
+    service_account = google_service_account.gke.email
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
