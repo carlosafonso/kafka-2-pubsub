fdd125b8d10679753aefdefc55abf71d27b26e18
Carlos Afonso <fonsi@google.com>
2022-07-12 07:59:51 +0000

    Modify producer and consumer to support an environment provided via the K2PS_ENV env var

--- src/consume.py
+++ src/consume.py
@@ -2,14 +2,15 @@
 import argparse
 from kafka2pubsub import get_bus, init_logging
 import logging
+import os
 
 
-def consume(consumer_name):
+def consume(consumer_name, env):
     def callback(data):
         logging.info("Received data: %s" % data)
 
     init_logging()
-    logging.info("Consumer '%s' initializing" % consumer_name)
+    logging.info("Consumer '%s' initializing, environment is '%s'" % (consumer_name, env))
 
     bus = get_bus()
     bus.subscribe(callback)
@@ -19,4 +20,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Consumes messages from an event bus.')
     parser.add_argument('consumer_name', type=str, help='The name of this consumer.')
     args = parser.parse_args()
-    consume(args.consumer_name)
+
+    env = 'undefined'
+    if 'K2PS_ENV' in os.environ:
+        env = os.environ['K2PS_ENV']
+
+    consume(args.consumer_name, env)
--- src/produce.py
+++ src/produce.py
@@ -2,18 +2,19 @@
 import argparse
 from kafka2pubsub import get_bus, init_logging
 import logging
+import os
 import time
 import uuid
 
 
-def produce(producer_name):
+def produce(producer_name, env):
     init_logging()
-    logging.info("Producer '%s' initializing" % producer_name)
+    logging.info("Producer '%s' initializing, environment is '%s'" % (producer_name, env))
 
     bus = get_bus()
 
     while True:
-        bus.publish({'from': producer_name, 'value': str(uuid.uuid4())})
+        bus.publish({'from': producer_name, 'value': str(uuid.uuid4()), 'env': env})
         logging.info("Message sent")
         time.sleep(1)
 
@@ -22,4 +23,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Publishes messages to an event bus.')
     parser.add_argument('producer_name', type=str, help='The name of this producer.')
     args = parser.parse_args()
-    produce(args.producer_name)
+
+    env = 'undefined'
+    if 'K2PS_ENV' in os.environ:
+        env = os.environ['K2PS_ENV']
+
+    produce(args.producer_name, env)
