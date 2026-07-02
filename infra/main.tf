eb8c5b2d0645a3c84cee0cb54b679a5a90440b59
Carlos Afonso <fonsi@google.com>
2026-07-02 20:18:00 +0200

    Add required services to Terraform template.

--- infra/main.tf
+++ infra/main.tf
@@ -23,9 +23,50 @@ provider "google-beta" {
   zone    = var.zone
 }
 
+resource "google_project_service" "sourcerepo" {
+  service            = "sourcerepo.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "clouddeploy" {
+  service            = "clouddeploy.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "compute" {
+  service            = "compute.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "pubsub" {
+  service            = "pubsub.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "artifactregistry" {
+  service            = "artifactregistry.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "container" {
+  service            = "container.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "cloudbuild" {
+  service            = "cloudbuild.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "iam" {
+  service            = "iam.googleapis.com"
+  disable_on_destroy = false
+}
+
 resource "google_compute_network" "vpc" {
   name                    = "kafka-2-pubsub"
   auto_create_subnetworks = "false"
+  depends_on              = [google_project_service.compute]
 }
 
 resource "google_compute_subnetwork" "subnetwork" {
@@ -65,7 +106,8 @@ resource "google_compute_firewall" "rules" {
 }
 
 resource "google_pubsub_topic" "topic" {
-  name = "kafka-2-pubsub"
+  name       = "kafka-2-pubsub"
+  depends_on = [google_project_service.pubsub]
 }
 
 resource "google_pubsub_subscription" "subscription_a" {
@@ -79,15 +121,17 @@ resource "google_pubsub_subscription" "subscription_b" {
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
-  provider = google-beta
-  location = var.region
+  provider      = google-beta
+  location      = var.region
   repository_id = "kafka-2-pubsub"
-  format = "DOCKER"
+  format        = "DOCKER"
+  depends_on    = [google_project_service.artifactregistry]
 }
 
 resource "google_service_account" "gke" {
   account_id   = "kafka-2-pubsub-gke"
   display_name = "kafka2pubsub - Service account for the GKE cluster that allows it to interact with Pub/Sub"
+  depends_on   = [google_project_service.iam]
 }
 
 resource "google_project_iam_member" "gke_pubsub_publisher" {
@@ -109,16 +153,18 @@ resource "google_project_iam_member" "gke_artifact_registry_viewer" {
 }
 
 resource "google_container_cluster" "cluster" {
-  name = "kafka-2-pubsub"
+  name     = "kafka-2-pubsub"
   location = var.region
-  network = google_compute_network.vpc.self_link
+  network  = google_compute_network.vpc.self_link
   subnetwork = google_compute_subnetwork.subnetwork.self_link
 
   remove_default_node_pool = true
-  initial_node_count = 1
+  initial_node_count       = 1
 
   networking_mode = "VPC_NATIVE"
   ip_allocation_policy {}
+
+  depends_on = [google_project_service.container]
 }
 
 resource "google_container_node_pool" "nodepool" {
@@ -137,6 +183,7 @@ resource "google_container_node_pool" "nodepool" {
 resource "google_service_account" "cloudbuild" {
   account_id   = "kafka-2-pubsub-cloudbuild"
   display_name = "kafka2pubsub - Service account for running Cloud Build builds"
+  depends_on   = [google_project_service.iam]
 }
 
 # To-Do: we probably want to narrow this down to just the required permissions,
@@ -149,38 +196,44 @@ resource "google_project_iam_member" "cloudbuild" {
 }
 
 resource "google_sourcerepo_repository" "source_repo" {
-  name = "kafka-2-pubsub"
+  name       = "kafka-2-pubsub"
+  depends_on = [google_project_service.sourcerepo]
 }
 
 resource "google_cloudbuild_trigger" "trigger" {
-  name = "kafka-2-pubsub"
+  name            = "kafka-2-pubsub"
   service_account = google_service_account.cloudbuild.id
 
   trigger_template {
-    repo_name = split("/repos/", google_sourcerepo_repository.source_repo.id)[1]
+    repo_name   = split("/repos/", google_sourcerepo_repository.source_repo.id)[1]
     branch_name = "master"
   }
 
-  filename = "cloudbuild.yaml"
+  filename   = "cloudbuild.yaml"
+  depends_on = [google_project_service.cloudbuild]
 }
 
 resource "google_clouddeploy_target" "prod" {
   location = var.region
-  name = "kafka-2-pubsub-prod"
+  name     = "kafka-2-pubsub-prod"
 
   gke {
     cluster = google_container_cluster.cluster.id
   }
+
+  depends_on = [google_project_service.clouddeploy]
 }
 
 resource "google_clouddeploy_delivery_pipeline" "pipeline" {
   location = var.region
-  name = "kafka-2-pubsub"
+  name     = "kafka-2-pubsub"
 
   serial_pipeline {
     stages {
       target_id = "kafka-2-pubsub-prod"
-      profiles = ["prod"]
+      profiles  = ["prod"]
     }
   }
+
+  depends_on = [google_project_service.clouddeploy]
 }
